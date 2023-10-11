2d6ac06b77050aaf3a05219301173c881627d744
sujal <sujalsharma0611@gmail.com>
2023-10-11 20:08:08 +0530

    learning js in browser

--- JS course/3.map,filter and reduce/map,filter,reduce.js
+++ JS course/3.map,filter and reduce/map,filter,reduce.js
@@ -1,7 +1,7 @@
 //map() - create new array and call function for each array element
 //similar to forEach but the difference is forEach work on exisiting array and map work by creating new array
 //since map create a new array map returns that new array as well
-//SYNTAX : aray_name.map(function_name(current_ele , index , array_name) = > { operations});
+//SYNTAX : aray_name.map(function_name = (current_ele , index , array_name) = > { operations});
 let arr = [1,2,4,6,4];
 console.log(`original array : ${arr}`);
 
@@ -27,10 +27,10 @@ console.log(`\nFiltered array: ${filtered_arr}`);
 // **result is then carried over to element1 and element2 would have next element from array
 //ex:
 let arr3 = [1,2,5,4,8,6];
-let new_arr3= arr3.reduce((element1 ,element2) =>{
-    let result= element1+element2 ;     // i.e add all numbers together
+let new_arr3= arr3.reduce((total ,element2) =>{
+    let result= total+element2 ;     // i.e add all numbers together
     return result;
-    //now result is sent to element1 and element 2 would have next element
-    //dry run: for first call element1=1 and element2=2 now result = 3 and result is sent to element1 , so now element1=3 and element2 = 5 
+    //now result is sent to total and element 2 would have next element
+    //dry run: for first call toal=1 and element2=2 now result = 3 and result is sent to total , so now total=3 and element2 = 5 
 });
 console.log(`\nreduced to(addition) : ${new_arr3}`);
---
+++ JS course/4.JS in browser/script.js
@@ -0,0 +1,3 @@
+alert("hello");
+let a = prompt("enter a number bc");
+alert(`input is ${a} and type is ${typeof a}`);
